64a6a29e770e1f8f19ce20189e1e03c95dbe717a
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2024-03-28 14:43:41 +0000

    WIP: Background Lines

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -64,6 +64,8 @@ export default function ({ text = true }) {
                 showWipeAnimation ? styles.wipeAnimation : ""
             }`}
         >
+            {/* <div className="fixed top-0 left-0 w-full h-full bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"> */}
+
             {!text ? (
                 <img
                     src="https://cdn3.kars.bio/assets/pfp.gif"
@@ -74,6 +76,7 @@ export default function ({ text = true }) {
             ) : (
                 <h1 className="text-white">{greetings[greetingIndex]}</h1>
             )}
+            {/* </div> */}
         </div>
     );
 }
